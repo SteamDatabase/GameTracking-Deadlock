2349f67b22a1f29ea3045573d76c7bdf26e88022
Wendy <freeman@steamdb.info>
2025-05-13 07:28:05 +0000

    5591 | 12 files | M game/citadel/bin/win64/client_strings.txt,  M game/citadel/bin/win64/server_strings.txt,  M game/citadel/pak01_dir.txt,  M game/citadel/pak01_dir/panorama/layout/citadel_ui_post_game_scoreboard.xml,  M game/citadel/pak01_dir/panorama/layout/hud_ability_icon.xml,  M game/citadel/pak01_dir/panorama/layout/hud_ability_icon_active.xml,  M game/citadel/pak01_dir/panorama/styles/citadel_shop_mods_build_category.css,  M game/citadel/pak01_dir/panorama/styles/citadel_ui_post_game_scoreboard.css,  M game/citadel/pak01_dir/scripts/abilities.vdata,  M game/citadel/pak01_dir/scripts/npc_units.vdata,  M game/citadel/steam.inf,  M update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+set -euo pipefail
 
 cd "${0%/*}"
 . ../common.sh
 
 echo "Processing Deadlock..."
 
+DUMPER_EXIT_CODE=0
+
 ProcessDepot ".dll"
 DeduplicateStringsFrom ".dll" "game/bin/win64/engine2_strings.txt" "game/bin/win64/tier0_strings.txt"
 ProcessVPK
 
+set +e
 while IFS= read -r -d '' file
 do
 	echo " > $file"
@@ -19,9 +23,12 @@ do
 		--output "$(echo "$file" | sed -e 's/\.vpk$/\//g')" \
 		--vpk_cache \
 		--vpk_decompile \
-		--vpk_extensions "txt,lua,kv3,db,gameevents,vcss_c,vjs_c,vts_c,vxml_c,vsndevts_c,vsndstck_c,vpulse_c,vdata_c" \
-	|| echo "Decompiler failed"
+		--vpk_extensions "txt,lua,kv3,db,gameevents,vcss_c,vjs_c,vts_c,vxml_c,vsndevts_c,vsndstck_c,vpulse_c,vdata_c"
+	if [[ "$DUMPER_EXIT_CODE" -eq 0 ]] && [[ $? -ne 0 ]]; then
+		DUMPER_EXIT_CODE=$?
+	fi
 done <   <(find . -type f -name "pak01_dir.vpk" -print0)
+set -e
 
 while IFS= read -r -d '' file
 do
@@ -31,6 +38,8 @@ done <   <(find . -type f -name "*.js" -print0)
 #ProcessToolAssetInfo
 FixUCS2
 
-CreateCommit "$(grep "ClientVersion=" game/citadel/steam.inf | grep -o '[0-9\.]*')" "$1"
+CreateCommit "$(grep "ClientVersion=" game/citadel/steam.inf | grep -o '[0-9\.]*')" "${1:-}"
 
 echo "Done"
+
+exit "$DUMPER_EXIT_CODE"
